6df5f9ffb1dce285b4a6d2f96a56ecf17cad8bb2
Dalila <dalilarustan@gmail.com>
2023-11-19 15:28:20 -0300

    cree funcion constructora

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-alert ("bienvenido/a");
+const Producto = function (nombre,precio,stock){
+    this.nombre=nombre
+    this.precio=precio
+    this.stock=stock
+}
+let producto1 = new Producto ("pollera corta",16000,6)
+let producto2 = new Producto ("camisa",12000,9)
+let producto3 = new Producto ("musculosa lisa",9200,12)
+let producto4 = new Producto ("pantalon",17000,8)
+let producto5 = new Producto ("pollera larga",16900,5)
+let producto6 = new Producto ("musculosaa estampada",9500,10)
 
-confirm("¿desea calcular monto en cuotas?")
-if(true){
-    function calcular(montoTotal,cuotas){
-        montoTotal=parseFloat(prompt("ingrese un monto determinado"));
-        while (montoTotal<2000){
-            alert("el monto debe ser de al menos $2000")
-            confirm("¿desea ingresar otro monto?")
-        if (confirm=true){
-            calcular()
-        }
-        }
-        cuotas=parseInt(prompt("ingrese cantidad de cuotas deseadas"));
-        let resultado= montoTotal / cuotas;
-        alert ("deberá pagar "+cuotas +" cuotas de "+resultado);
+let lista=[producto1,producto2,producto3,producto4,producto5,producto6]
+
+function filtrarProductos(){
+    let palabraClave= prompt("ingrese el producto que desea buscar").toLowerCase()
+    let resultado = lista.filter((x)=>x.nombre.toLowerCase().inculdes(palabraClave))
+    if (resultado.length >0){
+        console.table(resultado)
+    }else{
+        alert("no se encontró el producto "+ palabraClave)
     }
 }
 
-calcular()
+filtrarProductos()
