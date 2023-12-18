00d272b134ead9870a18a7709cbc9d86abdacc89
Dalila <dalilarustan@gmail.com>
2023-12-18 13:15:18 -0300

    agregado de imagenes

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const Producto = function (nombre,precio,stock){
+const Producto = function (nombre,precio,stock,imagen){
     this.nombre=nombre
     this.precio=precio
     this.stock=stock
+    this.imagen=imagen
 }
-let producto1 = new Producto ("pollera corta",16000,6)
-let producto2 = new Producto ("camisa",12000,9)
-let producto3 = new Producto ("musculosa lisa",9200,12)
-let producto4 = new Producto ("pantalon",17000,8)
-let producto5 = new Producto ("pollera larga",16900,5)
-let producto6 = new Producto ("musculosaa estampada",9500,10)
+let producto1 = new Producto ("pollera corta",16000,6,"/fotosJS/polleracorta.jpg")
+let producto2 = new Producto ("camisa",12000,9,"/fotosJS/camisa.jpg")
+let producto3 = new Producto ("musculosa lisa",9200,12,"/fotosJS/musculosa.jpg")
+let producto4 = new Producto ("pantalon",17000,8,"/fotosJS/pantalon.jpg")
+let producto5 = new Producto ("pollera larga",16900,5,"/fotosJS/polleralarga.jpg")
+let producto6 = new Producto ("musculosaa estampada",9500,10,"/fotosJS/musculosaestampada.jpg")
 
 let lista=[producto1,producto2,producto3,producto4,producto5,producto6]
 
@@ -43,6 +44,10 @@ function filtrarProductos(){
         const stock = document.createElement("p")
         stock.textContent = `quedan en stock: ${producto.stock}`
         card.appendChild(stock)
+
+        const imagen = document.createElement("img")
+        imagen.src = producto.imagen
+        card.appendChild(imagen)
 
         container.appendChild(card)
 
